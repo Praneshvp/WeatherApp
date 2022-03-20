bc7715fee70a0172456d2378911d2e4842325a9a
Pranesh V <pranesh@firesky.cc>
2022-03-20 12:56:38 +0530

    App.js

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 
 import React from 'react';
-import Main from './src/screens/Main';
+import { SafeAreaView } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './src/redux/store/store';
+import ListItems from './src/screens/ListItems';
+import Main from './src/screens/Main';
 const App = () => {
   return (
     <Provider store={store}>
-      <Main/>
+      <SafeAreaView>
+        <ListItems />
+      </SafeAreaView>
+
     </Provider>
   )
 };
